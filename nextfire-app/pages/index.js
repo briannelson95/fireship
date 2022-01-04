b59a55abe685d02649c30cc14a279342fde6522c
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-01-03 21:43:02 -0500

    end lesson 9

--- nextfire-app/pages/index.js
+++ nextfire-app/pages/index.js
@@ -1,11 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-     
+      <Link href={{
+        pathname: '/[username]',
+        query: { username: 'briann95' }
+      }}>
+        <a>Brian's Profile</a>
+      </Link>
     </div>
   )
 }
