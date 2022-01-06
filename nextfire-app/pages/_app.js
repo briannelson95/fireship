0d46c9cd28bbb9fea23aacbf36d0b39bdc1e31d6
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-01-06 06:24:23 -0500

    Update _app.js

--- nextfire-app/pages/_app.js
+++ nextfire-app/pages/_app.js
@@ -1,35 +1,35 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import { Toaster } from 'react-hot-toast'
-import UserContext from '../lib/context'
+import { UserContext } from '../lib/context'
 import { useState, useEffect } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, firestore } from '../lib/firebase'
 
 function MyApp({ Component, pageProps }) {
 
-  const [user] = useAuthState(auth)
-  const [username] = useState(null)
+  // const [user] = useAuthState(auth)
+  // const [username] = useState(null)
 
-  useEffect(() => {
-    let unsubscribe;
+  // useEffect(() => {
+  //   let unsubscribe;
     
-    if (user) {
-      const ref = firestore.collection('users').doc(user.uid)
-      unsubscribe = ref.onSnapshot((doc) => {
-        setUsername(doc.data()?.username)
-      });
-    } else {
-      setUsername(null)
-    }
+  //   if (user) {
+  //     const ref = firestore.collection('users').doc(user.uid)
+  //     unsubscribe = ref.onSnapshot((doc) => {
+  //       setUsername(doc.data()?.username)
+  //     });
+  //   } else {
+  //     setUsername(null)
+  //   }
 
-    return unsubscribe;
+  //   return unsubscribe;
 
-  }, [user])
+  // }, [user])
 
   return (
     <>
-      <UserContext.Provider value={{ user, username }}>
+      <UserContext.Provider value={{ user: [], username: 'brian' }}>
         <Navbar />
         <Component {...pageProps} />
         <Toaster />
