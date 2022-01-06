4e5dd70e441c415ee349a1e8204f185a7c4e11ce
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-01-06 08:25:07 -0500

    nothing makes sense

--- nextfire-app/pages/_app.js
+++ nextfire-app/pages/_app.js
@@ -2,34 +2,14 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import { Toaster } from 'react-hot-toast'
 import { UserContext } from '../lib/context'
-import { useState, useEffect } from 'react'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth, firestore } from '../lib/firebase'
+import { useUserData } from '../lib/hooks'
 
 function MyApp({ Component, pageProps }) {
-
-  // const [user] = useAuthState(auth)
-  // const [username] = useState(null)
-
-  // useEffect(() => {
-  //   let unsubscribe;
-    
-  //   if (user) {
-  //     const ref = firestore.collection('users').doc(user.uid)
-  //     unsubscribe = ref.onSnapshot((doc) => {
-  //       setUsername(doc.data()?.username)
-  //     });
-  //   } else {
-  //     setUsername(null)
-  //   }
-
-  //   return unsubscribe;
-
-  // }, [user])
+  const userData = useUserData();
 
   return (
     <>
-      <UserContext.Provider value={{ user: [], username: 'brian' }}>
+      <UserContext.Provider value={userData}>
         <Navbar />
         <Component {...pageProps} />
         <Toaster />
